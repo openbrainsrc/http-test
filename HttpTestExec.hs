4f4db1ad8ad9ede6072999b3d1e8b885a9b799a6
Tom Nielsen <tanielsen@gmail.com>
2014-05-20 15:31:17 +0100

    drop text bounds

--- HttpTestExec.hs
+++ HttpTestExec.hs
@@ -1,7 +1,22 @@
-import Test.HTTP
 import System.Environment
-import Data.List (isInfixOf)
-
+import qualified Network.Wreq 
+import qualified Network.Wreq
+import Control.Lens
+import Data.Aeson.Lens
+import Control.Monad
+import Network.URI
 main = do
    args <- getArgs
-   return ()
+   mapM_ dispatch $ filter notOption args
+
+dispatch file = case parseURI file of 
+                  Nothing -> runFile file 
+                  Just _ -> runHttp file
+
+
+
+
+isOption ('-':_) = True
+isOption _ = False
+
+notOption = not . isOption
